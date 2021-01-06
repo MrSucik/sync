09a1bd68875e962190a459e4e9a1e8165018cb5f
Daniel Suchan <mr.sucik@gmail.com>
2021-01-06 23:07:50 +0100

    Sync app - Preview mode progress bar

--- sync-app/src/Preview/MediaPreview.tsx
+++ sync-app/src/Preview/MediaPreview.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { LinearProgress } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 import { MediaModel } from "../definitions";
 import { useDownloadURL } from "./useDownloadURL";
 import { useTimeout } from "./useTimeout";
+import moment from "moment";
 
 interface Props {
   media: MediaModel;
@@ -10,22 +12,39 @@ interface Props {
 
 const MediaPreview: React.FC<Props> = ({ media, onMediaEnd }) => {
   useTimeout(media.duration, onMediaEnd);
+  const [progress, setProgress] = useState(0);
   const downloadURL = useDownloadURL(media.source);
-  return media.type === "image" ? (
-    <img
-      style={{
-        position: "absolute",
-        left: 0,
-        top: "50%",
-        right: 0,
-        width: "100%",
-        transform: "translateY(-50%)",
-      }}
-      src={downloadURL}
-      alt={media.name}
-    />
-  ) : (
-    <video src={downloadURL} />
+  useEffect(() => {
+    const startTime = moment();
+    const interval = setInterval(() => {
+      setProgress((moment().diff(startTime) / (media.duration * 1000)) * 100);
+    }, 100);
+    return () => clearInterval(interval);
+  }, [media.duration]);
+  return (
+    <>
+      <LinearProgress
+        color="secondary"
+        variant="determinate"
+        value={progress}
+      />
+      {media.type === "image" ? (
+        <img
+          style={{
+            position: "absolute",
+            left: 0,
+            top: "50%",
+            right: 0,
+            width: "100%",
+            transform: "translateY(-50%)",
+          }}
+          src={downloadURL}
+          alt={media.name}
+        />
+      ) : (
+        <video src={downloadURL} />
+      )}
+    </>
   );
 };
 
